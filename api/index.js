b82ee24d5b83257a46132a07ec054857f946d1ae
nuxmeg-max <nuxmeg@gmail.com>
2026-04-17 19:14:35 +0700

    Simplify response handling in search API

--- api/index.js
+++ api/index.js
@@ -8,13 +8,10 @@ app.use(cors());
 app.get('/api/search', async (req, res) => {
     const q = req.query.q;
     if (!q) return res.status(400).json([]);
-
     try {
         const response = await axios.get(`https://api.vreden.my.id/api/ytsearch?query=${encodeURIComponent(q)}`);
-        const hasil = response.data.result || [];
-        res.status(200).json(hasil);
+        res.status(200).json(response.data.result || []);
     } catch (error) {
-        console.error(error);
         res.status(500).json([]);
     }
 });
