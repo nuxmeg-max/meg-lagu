ab585a04958c6aa57d59b2a35c2590347a52b42e
nuxmeg-max <nuxmeg@gmail.com>
2026-04-17 19:06:30 +0700

    Refactor search API to use variable 'q' instead of 'query'

--- api/index.js
+++ api/index.js
@@ -6,13 +6,15 @@ const app = express();
 app.use(cors());
 
 app.get('/api/search', async (req, res) => {
-    const query = req.query.q;
-    if (!query) return res.status(400).send([]);
+    const q = req.query.q;
+    if (!q) return res.status(400).json([]);
 
     try {
-        const response = await axios.get(`https://api.vreden.my.id/api/ytsearch?query=${encodeURIComponent(query)}`);
-        res.status(200).json(response.data.result || []);
+        const response = await axios.get(`https://api.vreden.my.id/api/ytsearch?query=${encodeURIComponent(q)}`);
+        const hasil = response.data.result || [];
+        res.status(200).json(hasil);
     } catch (error) {
+        console.error(error);
         res.status(500).json([]);
     }
 });
